d7886284dd4199aa7389e804cc2cd7efa2657db8
Yukiix <pierre.zachary45@gmail.com>
2022-05-03 12:11:06 +0200

    main

--- main.cu
+++ main.cu
@@ -3,14 +3,17 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
-__global__ void vecadd( int * v1, int * v2 )
-{
-    auto tid = threadIdx.x;
-
-    v2[tid] += v1[tid];
-
+__global__ void grayscale( unsigned char * rgb, unsigned char * g, std::size_t cols, std::size_t rows ) {
+    auto i = blockIdx.x * blockDim.x + threadIdx.x;
+    auto j = blockIdx.y * blockDim.y + threadIdx.y;
+    if( i < cols && j < rows ) {
+        g[ j * cols + i ] = (
+                                    307 * rgb[ 3 * ( j * cols + i ) ]
+                                    + 604 * rgb[ 3 * ( j * cols + i ) + 1 ]
+                                    + 113 * rgb[  3 * ( j * cols + i ) + 2 ]
+                            ) / 1024;
+    }
 }
-
 
 int main()
 {
@@ -32,38 +35,24 @@ int main()
     std::cout << "Image Size : " << m_in.rows * m_in.cols << std::endl;
 
 
-    std::vector< int > v1( 10 );
-    std::vector< int > v2( 10 );
 
-    int * v1_d = nullptr;
-    int * v2_d = nullptr;
-
-    for( std::size_t i = 0 ; i < v1.size() ; ++i )
-    {
-        v1[ i ] =  5;
-        v2[ i ] =  4;
-    }
-
-    cudaMalloc( &v1_d, v1.size() * sizeof( int ) );
-    cudaMalloc( &v2_d, v2.size() * sizeof( int ) );
-
-    cudaMemcpy(v1_d, v1.data(), v1.size() * sizeof( int ), cudaMemcpyHostToDevice );
-    cudaMemcpy(v2_d, v2.data(), v2.size() * sizeof( int ), cudaMemcpyHostToDevice );
-
-    vecadd<<< 1, 10 >>>( v1_d, v2_d );
-
-    cudaMemcpy(v2.data(),v2_d, v2.size() * sizeof( int ), cudaMemcpyDeviceToHost );
-
-
-
-    for (size_t idex = 0; idex < v1.size(); idex++)
-        std::cout <<   v2[idex] << " ";
-    std::cout << std::endl;
-
-
-
-    cudaFree( v1_d );
-    cudaFree( v2_d );
+    auto rgb = m_in.data;
+    auto rows = m_in.rows;
+    auto cols = m_in.cols;
+    std::vector< unsigned char > g( rows * cols );
+    cv::Mat m_out( rows, cols, CV_8UC1, g.data() );
+    unsigned char * rgb_d;
+    unsigned char * g_d;
+    cudaMalloc( &rgb_d, 3 * rows * cols );
+    cudaMalloc( &g_d, rows * cols );
+    cudaMemcpy( rgb_d, rgb, 3 * rows * cols, cudaMemcpyHostToDevice );
+    dim3 t( 32, 32 );
+    dim3 b( ( cols - 1) / t.x + 1 , ( rows - 1 ) / t.y + 1 );
+    grayscale<<< b, t >>>( rgb_d, g_d, cols, rows );
+    cudaMemcpy( g.data(), g_d, rows * cols, cudaMemcpyDeviceToHost );
+    cv::imwrite( "../out.jpg", m_out );
+    cudaFree( rgb_d);
+    cudaFree( g_d);
 
     return 0;
 }
